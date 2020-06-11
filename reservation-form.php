9bb604bba052c590266714c7f40b880ee9ab0875
marine-jacquens <marine.jacquens@laplateforme.io>
2020-06-11 07:49:14 +0200

    essai conditions jour et heures

--- reservation-form.php
+++ reservation-form.php
@@ -14,27 +14,48 @@
             
             $date_min=date("Y-m-d");
             $date_max=date("Y-m-d", strtotime("+1 year"));
-            $heure_min=date("h:i:s");
-            $heure_max=date("h:i:s", strtotime('now +1 Hour'));
-            
-            
+            $heure=date("H");
+            $date=date("D");
+
             
             if(isset($_SESSION['login'])){
                 
                 if(isset($_POST['submit'])){
+                    
                 $titre=$_POST['title'];
                 $description=$_POST['description'];
                 $debut=$_POST['date_debut']." ".$_POST['heure_debut'];
                 $fin=$_POST['date_fin']." ".$_POST['heure_fin'];
+                
                     
-                    if($titre && $description && $debut && $fin){
+                    if($titre && $description && $debut){
                         
-                       
-                        $connexion=mysqli_connect('localhost','root','','reservationsalles');
-                        $requete="INSERT INTO reservations (titre,description,debut,fin) VALUES ('$titre','$description','$debut','$fin')";
-                        $execution=mysqli_query($connexion,$requete);
                         
-                        header('location:planning.php');
+                        
+                        // Condition du lundi au vendredi
+                        
+                        if($date!="Sat" && $date!="Sun"){
+                            if($date == (isset($_POST['date_debut']))){
+
+                       // Condition entre 8h et 19h
+                            if(isset($_POST['heure_debut'])==$heure){
+                                if($heure >= "08" && $heure<= "19"){
+
+                            // Condition 1h max de réservation
+                            
+                                $connexion=mysqli_connect('localhost','root','','reservationsalles');
+
+                                $requete="INSERT INTO reservations (titre,description,debut,fin) VALUES ('$titre','$description','$debut','$fin')";
+                                $execution=mysqli_query($connexion,$requete);
+                        
+                                header('location:planning.php');
+                                    
+                            }else echo "Réservation possible uniquement entre 08:00 et 19:00";
+                                
+                        }
+                            
+                            }else echo "Fermé le Samedi et Dimanche";
+                    }
                     }else echo "Veuillez remplir tous les champs";
 
                 }
@@ -46,6 +67,7 @@
         <main>
             <div class="content">
                 <?= renderErrors($errors) ?>
+                
                 <form action="reservation-form" method="post">
                     <label for="title">Titre</label><br>
                     <input type="text" name="title" id=title placeholder="titre"><br>
@@ -54,14 +76,16 @@
                     <textarea id="description" name="description"></textarea><br>
                     
                     <label for="date-debut">Date début</label>
-                    <input type="date" id="date-debut" name="date_debut" min="<?php echo $date_min; ?>" max="<?php echo $date_max; ?>">
+                    <input type="date" id="date_debut" name="date_debut" min="<?php $date_min; ?>" max="<?php $date_max; ?>">
                     
-                    <label for="heure-debut">Heure de début</label>
-                    <input type="time" id="heure" name="heure_debut"
-       min="<?php $heure_min; ?>" max="00:00"><br>
+                    <label for="heure-debut">de</label>
+                    <input type="time" id="heure_debut" name="heure_debut" min="<?php $heure_min; ?>"><br>
                     
-                    <label for="date-fin">Date de fin</label>
-                    <input type="date" id="date-fin" name="date_fin" min="<?php echo $date_min; ?>" max="<?php echo $date_max; ?>"><br>
+                    <label for="date-fin">Date fin</label>
+                    <input type="date" id="date_fin" name="date_fin" min="<?php $date_min; ?>" max="<?php $date_max; ?>" >
+                    
+                    <label for="heure-fin">à</label>
+                    <input type="time" id="heure_fin" name="heure_fin" ><br>
                     
                     
                     
