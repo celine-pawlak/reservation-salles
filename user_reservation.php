c222999f60f687e1f1be97a238592b752b36291f
Céline Pawlak <celine.pawlak@laplateforme.io>
2020-06-12 18:57:53 +0200

    user_reservation form update

--- user_reservation.php
+++ user_reservation.php
@@ -19,21 +19,35 @@
         $user_id = $_SESSION['user']['id'];
         $requete = "SELECT titre, description, debut, fin FROM reservations WHERE id_utilisateur = '$user_id'";
         $query = mysqli_query($db, $requete);
-        $user_reservations = mysqli_fetch_array($query);
+        $user_reservations = mysqli_fetch_all($query);
         var_dump($user_reservations);
         ?>
     </header>
     <main>
-<!--        <?php
-/*        foreach($user_reservations)
+        <?php
+        foreach($user_reservations as $key => $value)
             {
-                */?>
-                <form>
-
+                $titre = $user_reservations[$key][0];
+                $description = $user_reservations[$key][1];
+                $debut = $user_reservations[$key][2];
+                $heure_debut = date('H', $debut);
+                $date = date('d-m-Y', $debut);
+                $fin = $user_reservations[$key][3];
+                $heure_fin = date('H', $fin);
+                ?>
+                <p>Le <?= $date ?> de <?= $heure_debut ?> à <?= $heure_fin ?> :</p>
+                <form action="user_reservation.php" method="post">
+                    <label for="titre">Titre</label>
+                    <input type="text" name="titre" value="<?=$titre?>" >
+                    <label for="description">Description</label>
+                    <input type="text" name="description" value=<?=$description?>" >
+                    <button name="modify_reservation" type="submit">Modifier</button>
+                    <p>ou</p>
+                    <button type="submit"><i class="fad fa-trash-alt"></i></button>
                 </form>
-                --><?php
-/*            }
-        */?>
+                <?php
+            }
+        ?>
 
     </main>
     <footer>
