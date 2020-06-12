af8d73ecde77358a65b4327548c13eb49f355841
marine-jacquens <marine.jacquens@laplateforme.io>
2020-06-12 14:01:52 +0200

    reservation.php tableau ajouté , requête à revoir

--- reservation.php
+++ reservation.php
@@ -13,13 +13,61 @@
 <header>
     <?php include("header.php");
     $errors = [];
-    ?>
+    $user =  $_SESSION['user']['login'];
+
+    
+    if(isset($SESSION['$user'])){
+
+        
+        //CONNEXION BDD
+        $db=mysqli_connect('localhost','root','','reservationsalles');
+
+        //CREATION REQUETE
+        $requete="SELECT utilisateurs.login, reservations.titre, reservations.description, reservations.debut, reservations.fin FROM utilisateurs INNER JOIN reservations ON utilisateurs.id = reservations.id_utilisateur";
+        
+        //EXECUTION REQUETE
+        $query=mysqli_query($db,$requete);
+        
+        //RECUPERATION DONNEES
+        $fusion=mysqli_fetch_all($query); 
+    
+        var_dump($fusion);
+        
+    }
+        ?>
+        
+        
 
 </header>
 <main>
     <div class="content">
         <?= renderErrors($errors) ?>
     </div>
+    
+    <table>
+        <thead>
+            <tr>
+                <th>Nom du créateur</th>
+                <th>Titre de l'événement</th>
+                <th>Description</th>
+                <th>Heure de début</th>
+                <th>Heure de fin</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($fusion as $info){?>
+            <tr>
+                <td><?= $info [0] ?></td>
+                <td><?= $info [1] ?></td>
+                <td><?= $info [2] ?></td>
+                <td><?= $info [3] ?></td>
+                <td><?= $info [4] ?></td>
+            </tr><?php  } ?>
+        </tbody>
+    </table>
+    
+
+   
 </main>
 <footer>
     <?php include("footer.php") ?>
