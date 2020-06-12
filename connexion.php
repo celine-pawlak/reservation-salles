fcdd63f6f50cea81bc92f43e00ecac2a920e1270
marine-jacquens <marine.jacquens@laplateforme.io>
2020-06-12 11:51:49 +0200

    update connexion

--- connexion.php
+++ connexion.php
@@ -15,33 +15,6 @@ $page_selected = "conexion";
         <header>
           <?php include("header.php");
             $errors = [];
-            
-            if(isset($_POST['submit'])){
-                $login=$_POST['login'];
-                $password=$_POST['password'];
-                    
-                if($login && $password){
-                    $connexion=mysqli_connect('localhost','root','','reservationsalles');
-                    
-                    $requete="SELECT * FROM utilisateurs WHERE `login`='$login'";
-                    $execution=mysqli_query($connexion,$requete);
-                    
-                   $user=mysqli_fetch_array($execution); 
-                    var_dump($user);
-                    echo $password;
-                    
-                    var_dump(password_verify($password,$user[2]));
-                    
-                    var_dump($password,$user[2] );
-                    if(password_verify($password,$user[2])){
-                        
-                        $_SESSION['login']=$_POST['login'];
-                        header('location:planning.php');
-                        
-                            }else{echo "Login ou mot de passe incorrect";} 
-                    
-                        }else{echo "Veuillez remplir tous les champs";} 
-                    }
 
             ?>
         </header>
@@ -49,20 +22,7 @@ $page_selected = "conexion";
             <div class="content">
                 <?= renderErrors($errors) ?>
             </div>
-            <form action="connexion.php" method="post">
-                <h1> CONNEXION </h1><br/>
 
-                <label for="login">Identifiant</label>
-                <input type="text" id="login" name="login" placeholder="Créez votre pseudo"> <br/>
-
-                <label for="password">Mot de passe</label>
-                <input type="password" id="password" name="password" placeholder="Entrer un mot de passe"> <br/>
-
-                <div class="button" >
-                    <input type="submit" value="VALIDER" name="submit">
-                </div>
-
-             </form>
         </main>
         <footer>
           <?php include("footer.php")?>
