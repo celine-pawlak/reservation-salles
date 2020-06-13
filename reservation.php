e4ef41a3dd2e9703986206612164e7ae488afe0f
marine-jacquens <marine.jacquens@laplateforme.io>
2020-06-13 17:26:13 +0200

    update

--- reservation.php
+++ reservation.php
@@ -1,5 +1,6 @@
 <?php $page_selected = "reservation"; ?>
 
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
@@ -12,36 +13,33 @@
 <body>
 <header>
     <?php include("header.php");
-    $errors = [];
-    $user =  $_SESSION['user']['login'];
-
+    $errors = []; 
     
-    if(isset($_SESSION['$user'])){
-
+   
         
-        //CONNEXION BDD
-        $db = mysqli_connect('localhost','root','','reservationsalles');
-
-        //CREATION REQUETE
-        $requete = "SELECT utilisateurs.login, reservations.titre, reservations.description, reservations.debut, reservations.fin FROM utilisateurs INNER JOIN reservations ON utilisateurs.id = reservations.id_utilisateur";
+    $servname="localhost";
+    $dbname="reservationsalles";
+    $user="root";
+    
+    try{
+        $dbco=new PDO("mysql:host=$servname;dbname=$dbname",$user);
         
-        //EXECUTION REQUETE
-        $query = mysqli_query($db,$requete);
+        $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-        //RECUPERATION DONNEES
-        $fusion = mysqli_fetch_all($query); 
+        $sth = $dbco -> prepare("SELECT utilisateurs.login, reservations.titre, reservations.description, reservations.debut, reservations.fin FROM utilisateurs INNER JOIN reservations ON utilisateurs.id = reservations.id_utilisateur");
+        
+        $sth -> execute();
+        
+        $resultat = $sth->fetchAll(PDO::FETCH_ASSOC);
+            
         
     }
-        ?>
-        
-        
-
-</header>
-<main>
-    <div class="content">
-        <?= renderErrors($errors) ?>
-    </div>
     
+    catch(PDOException $e){
+                echo "Erreur : " . $e->getMessage();
+            }
+        
+        ?>
     <table>
         <thead>
             <tr>
@@ -53,22 +51,82 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($fusion as $info){?>
+            <?php foreach ($resultat as $info => $value){?>
             <tr>
-                <td><?= $info [0] ?></td>
-                <td><?= $info [1] ?></td>
-                <td><?= $info [2] ?></td>
-                <td><?= $info [3] ?></td>
-                <td><?= $info [4] ?></td>
-            </tr><?php  } ?>
+                <td><?= $value['login'] ?></td>
+                <td><?= $value['titre'] ?></td>
+                <td><?= $value['description'] ?></td>
+                <td><?= $value['debut'] ?></td>
+                <td><?= $value['fin'] ?></td>
+            </tr>
         </tbody>
     </table>
-    
+        <?php  
+    }
+            
+    ?>
 
-   
+        
+        
+
+</header>
+<main>
+    <div class="content">
+        <?= renderErrors($errors) ?>
+    </div>
+
 </main>
 <footer>
     <?php include("footer.php") ?>
 </footer>
 </body>
 </html>
+
+<!--
+    $user =  $_SESSION['user']['login'];
+
+    
+    if(isset($_SESSION['$user'])){
+
+        
+        //CONNEXION BDD
+        $db = mysqli_connect('localhost','root','','reservationsalles');
+
+        //CREATION REQUETE
+        $requete = "SELECT utilisateurs.login, reservations.titre FROM utilisateurs INNER JOIN reservations ON utilisateurs.id = reservations.id_utilisateur";
+        
+        //EXECUTION REQUETE
+        $query = mysqli_query($db,$requete);
+        
+        //RECUPERATION DONNEES
+        $fusion = mysqli_fetch_all($query); 
+        
+    }
+-->
+
+
+<!--
+
+    $servname="localhost";
+    $dbname="reservationsalles";
+    $user="root";
+    
+    try{
+        $dbco=new PDO("mysql:host=$servname;dbname=$dbname",$user);
+        
+        $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $sth = $dbco->prepare("SELECT utilisateurs.login, reservations.titre, reservations.description, reservations.debut, reservations.fin FROM utilisateurs INNER JOIN reservations ON utilisateurs.id = reservations.id_utilisateur");
+        
+        $sth -> execute();
+        
+        $resultat = $sth->fetchAll(PDO::FETCH_ASSOC);
+        
+        print_r($resultat);    
+    }
+    
+    catch(PDOException $e){
+                echo "Erreur : " . $e->getMessage();
+            }
+
+-->
