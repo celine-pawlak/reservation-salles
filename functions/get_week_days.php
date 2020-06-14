d4b3e8adc397d0a94fa97d4ec0fee288ff5cf559
Sabri SAID <sabri.said@laplateforme.io>
2020-06-14 10:43:50 +0200

    Update get_week_days function
    Translate week days to french

--- functions/get_week_days.php
+++ functions/get_week_days.php
@@ -8,16 +8,16 @@ function getWeekDays()
     setlocale(LC_TIME, 'fr_FR.UTF8');
     $nowDay = time();
 //    $weekDayStr = getdate($nowDay)['weekday'];
+//    $weekDays[] = strftime('%A', $nowDay);
     $weekDays[] = null;
+
 //    $nowDayJ1 = $nowDay + (60 * 60) * 24;
 //    $nowDayJ7 = $nowDay + (60 * 60 * 24) * 7;
 
-    for ($i = 1; $i < 12; ++$i) {
+    for ($i = 0; $i < 7; ++$i) {
         $nowDay = $nowDay + (60 * 60 * 24);
-        $weekDays[] = getdate($nowDay)['weekday'];
+        $weekDays[] = strftime('%A', $nowDay);
+//        $weekDays[] = getdate($nowDay)['weekday'];
     }
     return $weekDays;
 }
-
-$test = getWeekDays();
-var_dump($test);
